557d0977687c322528a53b3a3a5a7e21bcac0888
J5Mark <64222034+J5Mark@users.noreply.github.com>
2022-06-08 11:29:17 +0300

    updated some words and no bugs

---
+++ untitled.py
@@ -0,0 +1,16 @@
+words = [ 'chair', 'the pot', 'ganja', 'the crystals', 'my money', 'the guns', 'the children'] 
+mmenia = [ 'I', 'we', 'you', 'they', 'he', 'she'] #1
+vbs = ['are', 'am', 'is', 'aint'] #2
+F_bombs = ['sheesh', 'lick', 'make', 'blow', 'love', 'call', 'phone', 'shoot', 'doodoo', 'gonna'] #2
+import random as rd
+txt = ''
+
+for i in range(int(input())): #ho many senteces
+         sent = ' '
+         sent += rd.choice(mmenia) + ' '
+         sent += rd.choice([rd.choice(F_bombs), rd.choice(vbs)]) + ' '
+         if 'gonna' in sent:
+                  sent += rd.choice( ['sheesh', 'lick', 'make', 'blow', 'love', 'call', 'phone', 'shoot', 'doodoo']) + ' '
+         sent += rd.choice(words)
+         txt += sent + rd.choice(['.', '.','.','.','.','!', '!', '?'])
+print(txt + '\n This text is generated automatically by the machine called: "untitled"')
